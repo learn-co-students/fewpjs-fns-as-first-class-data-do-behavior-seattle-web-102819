89f1a0c12e86accf1c8881c66af8f7c20221feeb
AndrewMatheny <github email address>
2019-12-09 15:45:36 -0800

    start

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+    
+}
 /* Write your implementation of displayMessage() */
